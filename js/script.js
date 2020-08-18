c81cb4c78840fb21d4d80f8490a0477636a63aa6
Ivan Blažević <ivan.blazevic97@gmail.com>
2020-08-18 12:50:32 +0200

    složen dizajn - grid + strelice + carousel

--- js/script.js
+++ js/script.js
@@ -1,40 +1,52 @@
-var carousel = $('.carousel');
-var artikli = $('.artikl');
+$(document).ready(function() {
 
-function sljedeci(element) {
-    if (element.next().length > 0) {
-        return element.next();
-    } else {
-        return artikli.first();
-    }
-}
+    var url = 'http://newsapi.org/v2/top-headlines?' +
+          'sources=bbc-news&' +
+          'apiKey=1f3f0b7114fa4017be914ae300d76797';
+    var req = new Request(url);
+    fetch(req).then(function (response) {
+        console.log(response.json());
+    })
 
-function prethodni(element) {
-    if (element.prev().length > 0) {
-        return element.prev();
-    } else {
-        return artikli.last();
-    }
-}
+    var carousel = $('.carousel');
+    var artikli = $('.artikl');
 
-$('.strelica').on('click', function(strelica) {
-    var el = $('.ref').removeClass('ref');
-
-    if ($(strelica.currentTarget).data('toggle') == 'sljedeca') {
-        nova_vijest = sljedeci(el);
-        carousel.removeClass('tranzicija-unatrag');
-    } else {
-        nova_vijest = prethodni(el);
-        carousel.addClass('tranzicija-unatrag');
+    function sljedeci(element) {
+        if (element.next().length > 0) {
+            return element.next();
+        } else {
+            return artikli.first();
+        }
     }
 
-    nova_vijest.addClass('ref').css('order', 1);
-    for (i = 2; i <= artikli.length; i++) {
-        nova_vijest = sljedeci(nova_vijest).css('order', i);
+    function prethodni(element) {
+        if (element.prev().length > 0) {
+            return element.prev();
+        } else {
+            return artikli.last();
+        }
     }
 
-    carousel.removeClass('tranzicija');
-    setTimeout (function() {
-        carousel.addClass('tranzicija');
-    }, 50);
+    $('.strelica').on('click', function(strelica) {
+        var el = $('.ref').removeClass('ref');
+
+        if ($(strelica.currentTarget).data('toggle') == 'sljedeca') {
+            nova_vijest = sljedeci(el);
+            carousel.removeClass('tranzicija-unatrag');
+        } else {
+            nova_vijest = prethodni(el);
+            carousel.addClass('tranzicija-unatrag');
+        }
+
+        nova_vijest.addClass('ref').css('order', 1);
+        for (i = 2; i <= artikli.length; i++) {
+            nova_vijest = sljedeci(nova_vijest).css('order', i);
+        }
+
+        carousel.removeClass('tranzicija');  // ANIMACIJA SE OVDJE DOGAĐA
+        setTimeout (function() {
+            carousel.addClass('tranzicija');
+        }, 50);
+    });
+
 });
